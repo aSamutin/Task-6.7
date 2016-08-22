6c668775a73cc66d216cfba3977e73e2491258bb
aSamutin <a.samutin@initlab.ru>
2016-08-22 20:11:18 +0300

    Добавлен общий класс Figure

---
+++ src/Figure.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class Figure {
+
+    constructor(height, width, color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    render(){
+       let canvas = document.getElementById("canvas");
+       let ctx = canvas.getContext("2d");
+       ctx.fillStyle = this.color;
+       return ctx;
+    }
+}
+
+export {Figure};
